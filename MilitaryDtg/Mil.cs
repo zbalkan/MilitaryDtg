99b2cddfa4c8a00343bec654edb6b33a7e7fdb46
zbalkan <zafer@zaferbalkan.com>
2020-03-01 23:08:21 +0300

    Used expression bodied property where possible.

--- MilitaryDtg/Mil.cs
+++ MilitaryDtg/Mil.cs
@@ -14,31 +14,31 @@ namespace MilitaryDtg
         /// </summary>
         public struct Alphabet
         {            
-            public static string Alpha { get { return "Alpha"; } }
-            public static string Bravo { get { return "Bravo"; } }
-            public static string Charlie { get { return "Charlie"; } }
-            public static string Delta { get { return "Delta"; } }
-            public static string Echo { get { return "Echo"; } }
-            public static string Foxtrot { get { return "Foxtrot"; } }
-            public static string Golf { get { return "Golf"; } }
-            public static string Hotel { get { return "Hotel"; } }
-            public static string India { get { return "India"; } }
-            public static string Kilo { get { return "Kilo"; } }
-            public static string Lima { get { return "Lima"; } }
-            public static string Mike { get { return "Mike"; } }
-            public static string November { get { return "November"; } }
-            public static string Oscar { get { return "Oscar"; } }
-            public static string Papa { get { return "Papa"; } }
-            public static string Quebec { get { return "Quebec"; } }
-            public static string Romeo { get { return "Romeo"; } }
-            public static string Sierra { get { return "Sierra"; } }
-            public static string Tango { get { return "Tango"; } }
-            public static string Uniform { get { return "Uniform"; } }
-            public static string Victor { get { return "Victor"; } }
-            public static string Whiskey { get { return "Whiskey"; } }
-            public static string Xray { get { return "X-ray"; } }
-            public static string Yankee { get { return "Yankee"; } }
-            public static string Zulu { get { return "Zulu"; } }            
+            public static string Alpha => "Alpha";
+            public static string Bravo => "Bravo";
+            public static string Charlie => "Charlie";
+            public static string Delta => "Delta";
+            public static string Echo => "Echo";
+            public static string Foxtrot => "Foxtrot";
+            public static string Golf => "Golf";
+            public static string Hotel => "Hotel";
+            public static string India => "India";
+            public static string Kilo => "Kilo";
+            public static string Lima => "Lima";
+            public static string Mike => "Mike";
+            public static string November => "November";
+            public static string Oscar => "Oscar";
+            public static string Papa => "Papa";
+            public static string Quebec => "Quebec";
+            public static string Romeo => "Romeo";
+            public static string Sierra => "Sierra";
+            public static string Tango => "Tango";
+            public static string Uniform => "Uniform";
+            public static string Victor => "Victor";
+            public static string Whiskey => "Whiskey";
+            public static string Xray => "X-ray";
+            public static string Yankee => "Yankee";
+            public static string Zulu => "Zulu";
         }
 
         /// <summary>
@@ -46,31 +46,31 @@ namespace MilitaryDtg
         /// </summary>
         public struct TimeZoneAbbreviation
         {
-            public static string A { get { return "A"; } }
-            public static string B { get { return "B"; } }
-            public static string C { get { return "C"; } }
-            public static string D { get { return "D"; } }
-            public static string E { get { return "E"; } }
-            public static string F { get { return "F"; } }
-            public static string G { get { return "G"; } }
-            public static string H { get { return "H"; } }
-            public static string I { get { return "I"; } }
-            public static string K { get { return "K"; } }
-            public static string L { get { return "L"; } }
-            public static string M { get { return "M"; } }
-            public static string N { get { return "N"; } }
-            public static string O { get { return "O"; } }
-            public static string P { get { return "P"; } }
-            public static string Q { get { return "Q"; } }
-            public static string R { get { return "R"; } }
-            public static string S { get { return "S"; } }
-            public static string T { get { return "T"; } }
-            public static string U { get { return "U"; } }
-            public static string V { get { return "V"; } }
-            public static string W { get { return "W"; } }
-            public static string X { get { return "X"; } }
-            public static string Y { get { return "Y"; } }
-            public static string Z { get { return "Z"; } }
+            public static string A => "A";
+            public static string B => "B";
+            public static string C => "C";
+            public static string D => "D";
+            public static string E => "E";
+            public static string F => "F";
+            public static string G => "G";
+            public static string H => "H";
+            public static string I => "I";
+            public static string K => "K";
+            public static string L => "L";
+            public static string M => "M";
+            public static string N => "N";
+            public static string O => "O";
+            public static string P => "P";
+            public static string Q => "Q";
+            public static string R => "R";
+            public static string S => "S";
+            public static string T => "T";
+            public static string U => "U";
+            public static string V => "V";
+            public static string W => "W";
+            public static string X => "X";
+            public static string Y => "Y";
+            public static string Z => "Z";
         }
 
         #region Military Time Zone Names
