dc76c7bba7d7e3a181debe743b1a560b39b6e2c9
zbalkan <zafer@zaferbalkan.com>
2020-03-01 23:41:18 +0300

    Resharper cleanup

--- MilitaryDtg.Test/MilDateFormatProviderTests.cs
+++ MilitaryDtg.Test/MilDateFormatProviderTests.cs
@@ -9,14 +9,12 @@ namespace MilitaryDtg.Test
         [TestMethod]
         public void FormatTest()
         {
-            var dtgString = "07142509 Z OCT 2017";
+            const string dtgString = "07142509 Z OCT 2017";
             var format = "{0:" + Settings.Default.DefaultDateTimeGroupStringFormat + "}";
             var mdto = DateTimeMil.GetMilDateFromString(dtgString);
-            if (mdto.MilDateOffset.HasValue)
-            {
-                var mdtoString = string.Format(new MilDateFormatProvider(), format, mdto).ToUpper();
-                Assert.AreEqual(dtgString, mdtoString);
-            }
+            if (!mdto.MilDateOffset.HasValue) return;
+            var mdtoString = string.Format(new MilDateFormatProvider(), format, mdto).ToUpper();
+            Assert.AreEqual(dtgString, mdtoString);
         }
     }
 }
